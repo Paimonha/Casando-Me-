839a7890a28392e6854073cc189379fbdd9ac7cb
Paimonha <125074784+Paimonha@users.noreply.github.com>
2024-11-05 19:29:40 -0300

    Add files via upload

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,8 +1,47 @@
 import "./Login.css"
-function Login(){
-    return(
+import Logo from "./Logo.svg"
+import Icone from "./Icone.svg"
+function Login() {
+    return (
         <>
-     
+            <div id="P">
+                <div id="Cabecalho">
+                    <div id="Titulo">
+
+                        <span id="Span">Login</span>
+                    </div>
+                    <div id="Logo">
+                        <img src={Logo} alt="" />
+
+
+                    </div>
+                </div>
+
+                    <div id="Login">
+                        <div id="Icone">
+                            <img src={Icone} alt="" />
+
+                        </div>
+
+                        <div id="Inputs">
+
+                            <input type="text" placeholder="E-mail"/>
+                            <input type="password" placeholder="Password"/>
+
+                        </div>
+                        <p>Esqueci minha senha</p>
+
+                        
+                        
+
+
+                    </div>
+
+
+                
+            </div>
+
+
         </>
     )
 }
