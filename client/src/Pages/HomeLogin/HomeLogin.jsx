4a507e0f4a53edc46b2fbfeba8e0bdce1351e390
Ianlucca14 <124889802+Ianlucca14@users.noreply.github.com>
2024-11-14 09:01:50 -0300

    Update da Tela de Login
    Troca da Logo, ainda falta a responsividade

--- client/src/Pages/HomeLogin/HomeLogin.jsx
+++ client/src/Pages/HomeLogin/HomeLogin.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Header from "../../../Components/Header/Header";
 import Logo from "../../Svg/Titulo.svg";
+import { useState } from "react";
 import "./HomeLogin.css";
 
 function HomeLogin() {
@@ -10,12 +11,19 @@ function HomeLogin() {
         <Header />
 
         <div id="logo">
-          <img src={Logo} alt="" />
+          <img src="../../public/Logo3.svg" alt="" />
         </div>
+        
+<div className="Titulo">
+  <img src="../../public/Titulo_Img.svg" alt="" />
+</div>
+
         <div id="texto">
           <span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy{" "}
+            Bem-vindo ao Casando-Me! Realize seu cadastro e tenha acesso aos
+            nossos parceiros, incluindo cerimonialistas e locais para eventos.
+            Através da nossa plataforma, você poderá enviar mensagens
+            diretamente a esses profissionais para planejar o seu grande dia.
           </span>
         </div>
         <div id="botao">
