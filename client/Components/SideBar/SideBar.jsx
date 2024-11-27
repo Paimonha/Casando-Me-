dd8b02764dc4ab76abc7bd93365e412cb24a879b
Ricardo André <124917037+RicardoAndre69@users.noreply.github.com>
2024-11-27 12:40:32 -0300

    Add files via upload

--- client/Components/SideBar/SideBar.jsx
+++ client/Components/SideBar/SideBar.jsx
@@ -1,27 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBox, faChurch, faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import Logo from '../../src/Svg/Logo.svg'
-import './SideBar.css'
+import {
+  faChurch,
+  faUser ,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import Logo from "../../src/Svg/Logo.svg";
+import "./SideBar.css";
+import { useState } from "react";
 
 function Sidebar() {
-    return (
-        <>
-        <div id="D">
-            <div id="Side">
-                <div id="MetadeIcon" >
-                    <img src={Logo} alt="" />
+  const navigate = useNavigate();
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/rota/logout");
+      localStorage.removeItem("authToken");
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.error("Erro ao fazer logout", error);
+    }
+  };
+
+  const toggleSidebar = () => {
+    setIsMinimized(!isMinimized);
+  };
+
+  return (
+    <div id="D">
+      <div id="Side" className={isMinimized ? "minimized" : ""}>
+        <div id="MetadeIcon">
+          {/* Logo visível apenas quando não minimizada */}
+          {!isMinimized && <img src={Logo} alt="Logo" />}
           
-                    <div className="DivBox"> <Link to="/adminc"> <FontAwesomeIcon className="Box" icon={faChurch} /> </Link></div>
-                    <div className="DivBox"> <Link to="/adminu"> <FontAwesomeIcon className="Box" icon={faUser} /> </Link></div>
-                </div>
-                <div id="MetadeLogout">
-                        <div className="DivBox"> <Link to="/login"> <FontAwesomeIcon className="Box" icon={faRightFromBracket}rotation={180}  /> </Link></div>
-                </div>
-            </div>
+          {/* Botão para alternar minimização */}
+
+          <div className="DivBox">
+            <Link to="/adminc">
+              <FontAwesomeIcon className="Box" icon={faChurch} />
+            </Link>
+          </div>
+          <div className="DivBox">
+            <Link to="/adminu">
+              <FontAwesomeIcon className="Box" icon={faUser } />
+            </Link>
+          </div>
         </div>
-        </>
-    )
+        <div id="MetadeLogout">
+          <div className="DivBox">
+            <Link to="">
+              <FontAwesomeIcon
+                className="Box"
+                icon={faRightFromBracket}
+                rotation={180}
+                onClick={handleLogout}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
