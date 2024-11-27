985376f12cb8f8917dc430eeb11935f27ee6e586
Ianlucca14 <124889802+Ianlucca14@users.noreply.github.com>
2024-11-27 10:19:07 -0300

    Add files via upload

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,92 +1,89 @@
-import "./Login.css"
+import "./Login.css";
 import { Link } from "react-router-dom";
-import Logo from "../../Svg/Logo.svg"
-import Icone from "../../Svg/Icone.svg"
-import { useNavigate } from 'react-router-dom';
+import Logo from "../../Svg/Logo.svg";
+import Icone from "../../Svg/Icone.svg";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post('http://localhost:5000/api/rota/login', {
-                email,
-                password,
-            });
-
-            const { token, role } = response.data;
-
-            // Armazenar o token no localStorage ou sessionStorage
-            localStorage.setItem('token', token);
-
-            // Redirecionar o usuário dependendo do papel
-            if (role === 'admin') {
-                navigate('/admin');
-            } else {
-                navigate('/home');
-            }
-        } catch (error) {
-            console.error('Erro ao fazer login', error);
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/rota/login",
+        {
+          email,
+          password,
         }
-    };
+      );
 
+      const { token, role } = response.data;
 
-    return (
-        <>
-            <div id="P">
-                <div id="Cabecalho">
-                    <div id="TituloL">
+      // Armazenar o token no localStorage ou sessionStorage
+      localStorage.setItem("token", token);
 
-                        <span id="Span">Login</span>
-                    </div>
-                    <div id="Logo">
-                        <img src={Logo} alt="" />
+      // Redirecionar o usuário dependendo do papel
+      if (email === "ian@gmail.com") {
+        navigate("/admin");
+      } else {
+        navigate("/home");
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login", error);
+    }
+  };
 
+  return (
+    <>
+      <div id="P">
+        <div id="Cabecalho">
+          <div id="TituloL">
+            <span id="Span">Login</span>
+          </div>
+          <div id="Logo">
+            <img src={Logo} alt="" />
+          </div>
+        </div>
 
-                    </div>
-                </div>
+        <div id="Login">
+          <div id="Icone">
+            <img src={Icone} alt="" />
+          </div>
 
-                <div id="Login">
-                    <div id="Icone">
-                        <img src={Icone} alt="" />
-
-                    </div>
-
-                    <div >
-                        <form onSubmit={handleLogin} id="Inputs">
-                        <input type="email" placeholder="E-mail" value={email}  onChange={(e) => setEmail(e.target.value)}  required />
-                        <input type="senha" placeholder="Senha" value={senha}  onChange={(e) => setSenha(e.target.value)}  required  />
-
-
-                        </form>
-
-                        
-                    </div>
-                  {/*   <p>Esqueci minha <Link to="#"> senha</Link></p> */}
-                      <br />
-                    <div id="BtnLogar">
-                        <button>Logar</button>
-               
-                    </div>
-                    <Link to="/cadastro"><p  id="LinkC">Já tem Cadastro?</p></Link>
-
-
-
-
-                </div>
-
-
-
-            </div>
-
-
-        </>
-    )
+          <div>
+            <form onSubmit={handleLogin} id="Inputs">
+              <input
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                type="senha"
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                required
+              />
+            </form>
+          </div>
+          {/*   <p>Esqueci minha <Link to="#"> senha</Link></p> */}
+          <br />
+          <div id="BtnLogar">
+            <button>Logar</button>
+          </div>
+          <Link to="/cadastro">
+            <p id="LinkC">Já tem Cadastro?</p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Login
+export default Login;
